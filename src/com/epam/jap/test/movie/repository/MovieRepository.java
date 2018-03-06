074457361ee6d58cdec835f3981639af5b5e41c5
Tomasz S <tomasz.z.seidler@gmail.com>
2018-03-06 12:34:14 +0100

    missing tests supplement

--- src/com/epam/jap/test/movie/repository/MovieRepository.java
+++ src/com/epam/jap/test/movie/repository/MovieRepository.java
@@ -2,4 +2,12 @@ package com.epam.jap.test.movie.repository;
 
 public interface MovieRepository {
     public void loadRepository(String location);
+
+    public int movieCount();
+
+    public MovieRepository filterByRatingBetterThan(double rating);
+
+    public MovieRepository filterByYearOfProduction(int year);
+
+    public MovieRepository filterByRatingCountMoreThan(int ratingCount);
 }
