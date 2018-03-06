44461092828e26549296cf2b0b9ff4aab8522f26
Tomasz S <tomasz.z.seidler@gmail.com>
2018-03-06 12:43:50 +0100

    added exception handling when line cannot be parsed, reading of the movies file successful

--- src/com/epam/jap/test/movie/MovieFinder.java
+++ src/com/epam/jap/test/movie/MovieFinder.java
@@ -1,10 +1,16 @@
 package com.epam.jap.test.movie;
 
+import com.epam.jap.test.movie.repository.InMemoryMovieRepository;
+import com.epam.jap.test.movie.repository.MovieRepository;
+
 import java.util.Scanner;
 
 public class MovieFinder {
 
     public static void main(String[] args) {
+        MovieRepository movieRepository = new InMemoryMovieRepository();
+        movieRepository.loadRepository("src/com/epam/jap/test/movie/resources/movies.txt");
+        System.out.println("Loaded " + movieRepository.movieCount() + " movies");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Your command?");
